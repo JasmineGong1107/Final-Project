12cb2cbf223b8681d6c596991adbd659f2a06318
Jiaying <jgong1@babson.edu>
2020-11-25 22:15:57 +0800

    User Account and Profile Pic

--- notescloud/app/forms.py
+++ notescloud/app/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 import email_validator
@@ -40,4 +42,34 @@ class LoginForm(FlaskForm):
 
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
+    picture = FileField(
+        "Update Profile Picture", validators=[FileAllowed(["jpg", "png"])]
+    )
     submit = SubmitField("Login")
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=25)]
+    )
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField(
+        "Update Profile Picture", validators=[FileAllowed(["jpg", "png"])]
+    )
+
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    "This username is not longer available. Please use another one."
+                )
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("This email is taken. Please use another one.")
